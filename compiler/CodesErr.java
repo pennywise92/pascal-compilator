f126d6bd24e2d0a3bcaa303621095dc255a43e80
mezouari <mezouariadam8@gmail.com>
2023-04-30 18:36:57 +0100

    my pascal project

---
+++ compiler/CodesErr.java
@@ -0,0 +1,23 @@
+package net.mips.compiler;
+
+public enum CodesErr {
+	CAR_INC_ERR("Symbole inconnu"),
+	FIC_VID_ERR("Erreur d'ouverture de fichier");
+	
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	private CodesErr(String message) {
+		this.message=message;
+	}
+}
+
+
+
